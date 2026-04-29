b3d3cf7d2d6961b406ffde81f0301d26e0f1890c
alexandretrotel <trotelalexandre@proton.me>
2026-04-29 09:52:16 -0400

    fix: rework delete and set commands

--- src/commands/token/delete.rs
+++ src/commands/token/delete.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 
 use crate::services::token_store::GithubTokenStore;
 
-pub fn run(store: &GithubTokenStore) -> Result<()> {
+pub fn run() -> Result<()> {
+    let store = GithubTokenStore::new()?;
     store.delete()
 }
--- src/commands/token/set.rs
+++ src/commands/token/set.rs
@@ -3,6 +3,7 @@ use anyhow::Result;
 use crate::cli::args::SetTokenArgs;
 use crate::services::token_store::GithubTokenStore;
 
-pub fn run(store: &GithubTokenStore, args: SetTokenArgs) -> Result<()> {
+pub fn run(args: SetTokenArgs) -> Result<()> {
+    let store = GithubTokenStore::new()?;
     store.set(&args.token)
 }
